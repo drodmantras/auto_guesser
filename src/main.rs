85a0d6525ae92808ff957dfe1fdad8fa3d4888f8
Erik <no@mail.com>
2020-04-29 19:45:20 +0200

    vnos mej

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 use std::cmp::Ordering;
 use rand::Rng;
+use std::io;
 
 fn main() {
 
-    let skrivnost = rand::thread_rng().gen_range(1, 101);
+    println!("vnesi spodnjo mejo: ");
+    let mut spodnja = vnos();
+    println!("vnesi zgornjo mejo: ");
+    let mut zgornja = vnos();
+    print!("\n");
+
+    let skrivnost = rand::thread_rng().gen_range(spodnja, zgornja);
     println!("generirana={}", skrivnost);
-    let mut osnova: i32 = rand::thread_rng().gen_range(1, 101);
-    let mut spodnja = 1;
-    let mut zgornja = 101;
+    let mut osnova: i32 = rand::thread_rng().gen_range(spodnja, zgornja);
 
     let mut tries = 0;
+
     loop {
+
         tries+=1;
 
         match osnova.cmp(&skrivnost) {
@@ -35,4 +42,25 @@ fn main() {
         }
 
     }
+}
+
+fn vnos() -> i32 {
+
+    println!("(stevilo naj je pozitivno)");
+    let mut vn = String::new();
+
+    io::stdin()
+        .read_line(&mut vn)
+        .expect("napaka branja!");
+
+    let vn = vn.trim().parse::<i32>().expect("napačen vnos!");
+
+    if vn < 1 {
+        println!("Napaka! stevilo mora biti pozitivno! ");
+        let mut vn = vnos();
+        vn
+    } else {
+        vn
+    }
+
 }
